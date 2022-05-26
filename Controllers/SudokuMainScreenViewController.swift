6ecc595aa3aaa29571045d375ed051112f55053e
dabid <abd5@umbc.edu>
2022-05-26 15:14:50 -0400

    Able to generate random sudoku boards, need to save the board and fill the board

--- Controllers/SudokuMainScreenViewController.swift
+++ Controllers/SudokuMainScreenViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class SudokuMainScreenViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let N:Int = 9
+        let K:Int = 20
+        let sudoku = Sudoku(numberOfRowsColumns: N, numberOfMissingDigits: K);
+        sudoku.fillValues();
+        sudoku.printBoard();
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Utils/Sudoku.swift
+++ Utils/Sudoku.swift
@@ -6,20 +6,23 @@
 //
 
 import Foundation
-
 class Sudoku{
-    var mat: [[Int]] = []
+    var mat: [[Int]] = [[]]
     var numberOfRowsColumns: Int// Number of rows and columns
     var numberOfMissingDigits: Int //Number of digits missing
-    var sqrt: Int = 0
+    var squareRootValue: Int = 0
     
     init(numberOfRowsColumns: Int, numberOfMissingDigits: Int){
         self.numberOfMissingDigits = numberOfMissingDigits
         self.numberOfRowsColumns = numberOfRowsColumns
+        self.squareRootValue = Int(sqrt(Double(numberOfRowsColumns)))
+        mat = Array(repeating: Array(repeating: 0, count: numberOfRowsColumns), count: numberOfRowsColumns)
     }
     func fillValues(){
         fillDiagonal();
-        fillRemaining(row: 0,column: sqrt)
+        var zeroIndex = 0
+        var dummyVal = squareRootValue
+        fillRemaining(row: &zeroIndex,column: &dummyVal)
         removeKDigits()
     }
     
@@ -27,69 +30,143 @@ class Sudoku{
         var index:Int = 0
         while index < numberOfRowsColumns{
             fillBox(row: index, column: index)
-            index = index + sqrt
+            index = index + squareRootValue
         }
     }
     
-    func fillRemaining(row: Int, column: Int){
-        
+    func unUsedInBox(row: Int, column: Int, num: Int) -> Bool{
+        var rowIndex:Int = 0
+        while (rowIndex < squareRootValue - 1) {
+            var columnIndex:Int = 0
+            while (columnIndex < squareRootValue - 1) {
+                if (mat[row + rowIndex][column + columnIndex] == num){
+                    return false
+                }
+                columnIndex+=1
+            }
+            rowIndex+=1
+        }
+        return true
     }
     
-    func removeKDigits(){
-        var count: Int = numberOfMissingDigits
-        while (count != 0){
-            let cellId = randomGenerator(num: (numberOfRowsColumns * numberOfRowsColumns)) - 1
+    func fillBox(row: Int, column: Int){
+        var num: Int
+        var rowIndex:Int = 0
+        while (rowIndex < squareRootValue - 1) {
+            var columnIndex:Int = 0
+            while (columnIndex < squareRootValue - 1) {
+                repeat{
+                    num = randomGenerator(numberOfRowsColumns)
+                    
+                }while !unUsedInBox(row: row, column: column, num: num)
+                mat[row + rowIndex][column + columnIndex] = num
+                columnIndex+=1
+            }
+            rowIndex+=1
         }
+    }
+    
+    func randomGenerator(_ n:Int) -> Int {
+        return Int(arc4random_uniform(UInt32(n)))
+    } // end
+    func checkIfSafe(row: Int, column: Int, num: Int) -> Bool{
+        return (unUsedInRow(row: row, number: num) && unUsedInColumn(column: column, number: num) && unUsedInBox(row: row - row%squareRootValue, column: column - column%squareRootValue, num: num))
     }
     
     func unUsedInRow(row: Int, number: Int) -> Bool{
-        for columnIndex in (0 ..< numberOfRowsColumns){
+        var columnIndex:Int = 0
+        while (columnIndex < numberOfRowsColumns){
             if(mat[row][columnIndex] == number){
                 return false
             }
+            columnIndex+=1
         }
         return true
     }
     
     func unUsedInColumn(column: Int, number: Int) -> Bool{
-        for rowIndex in (0 ..< numberOfRowsColumns){
+        var rowIndex:Int = 0
+        while (rowIndex < numberOfRowsColumns){
             if(mat[rowIndex][column] == number){
                 return false
             }
+            rowIndex+=1
         }
         return true
     }
+   
     
-    func checkIfSafe(row: Int, column: Int, num: Int) -> Bool{
-        return (unUsedInRow(row: row, number: num) && unUsedInColumn(column: column, number: num) && unUsedInBox(row: row - row%sqrt, column: column - column%sqrt, num: num))
-    }
     
-    func fillBox(row: Int, column: Int){
-        var num: Int
-        for rowIndex in (0 ..< sqrt) {
-            for columnIndex in (0 ..< sqrt){
-                repeat{
-                    num = randomGenerator(num: numberOfRowsColumns)
-                    
-                }while !unUsedInBox(row: row, column: column, num: num)
-                mat[row + rowIndex][column + columnIndex] = num
-            }
+    
+    
+    func fillRemaining(row: inout Int, column: inout Int) -> Bool{
+        if (column >= numberOfRowsColumns && row < numberOfRowsColumns - 1){
+            row+=1
+            column = 0
         }
+            if (row >= numberOfRowsColumns && column >= numberOfRowsColumns){
+                return true
+            }
+            if (row < squareRootValue){
+                if (column < squareRootValue){
+                    column = squareRootValue
+                }
+            } else if (row < numberOfRowsColumns - squareRootValue){
+                if (column == Int(row/squareRootValue) * squareRootValue){
+                    column = column + squareRootValue
+                }
+            } else {
+                if (column == numberOfRowsColumns - squareRootValue){
+                    row+=1
+                    column = 0
+                    if (row >= numberOfRowsColumns){
+                        return true
+                    }
+                }
+            }
+            var num: Int = 1
+            while (num <= numberOfRowsColumns){
+                if (checkIfSafe(row: row, column: column, num: num)){
+                    mat[row][column] = num
+                    column+=1
+                    if (fillRemaining(row: &row, column: &column)){
+                        return true
+                    }
+                    mat[row][column] = 0;
+                }
+                num+=1
+            }
+        return false;
     }
     
-    func unUsedInBox(row: Int, column: Int, num: Int) -> Bool{
-        for rowIndex in (0 ..< sqrt) {
-            for columnIndex in (0 ..< sqrt){
-                if (mat[row + rowIndex][column + columnIndex] == num){
-                    return false
+    
+    func removeKDigits(){
+        var count: Int = numberOfMissingDigits
+        while (count != 0){
+            let cellId = randomGenerator((numberOfRowsColumns * numberOfRowsColumns))
+            let i = Int(cellId / numberOfRowsColumns)
+            var j = cellId % 9
+            if (j != 0){
+                j = j - 1
+                if (mat[i][j] != 0 ){
+                    count-=1
+                    mat[i][j] = 0
                 }
             }
         }
-        return true
     }
     
-    func randomGenerator(num: Int) -> Int{
-        let random = Double(arc4random()) * Double(num + 1)
-        return Int(random)
+    func printBoard(){
+        var i:Int = 0
+        while (i < numberOfRowsColumns) {
+            var j:Int = 0
+            var row:String = ""
+            while (j < numberOfRowsColumns) {
+                row.append("\(mat[i][j]) ")
+                j+=1
+            }
+            i+=1
+            print(row)
+        }
     }
 }
