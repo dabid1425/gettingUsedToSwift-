6a4bd5025416b3a58c14f3c2c48fd3bf9db4f16e
danabid <abd5@umbc.edu>
2022-07-21 09:29:57 -0400

    ON FUCKING DIFFERENT LAYER MOTHER FUCKER!!!!

--- Controllers/DrawViewController.swift
+++ Controllers/DrawViewController.swift
@@ -27,7 +27,7 @@ class DrawViewController: UIViewController,UICollectionViewDelegate, UICollectio
     var kHeight: CGFloat = 130 // Total height of feature view
     var animationTime = 0.35
     var currenlyMovingPoint: Bool = false
-    
+    var panRecognizer: UIPanGestureRecognizer!
     var colorsArray: [UIColor] = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 1, green: 0.4059419876, blue: 0.2455089305, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 1, green: 0.4059419876, blue: 0.2455089305, alpha: 1), #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.3823936913, green: 0.8900789089, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 0.4528176247, blue: 0.4432695911, alpha: 1), #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1), #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)]
     
     override func viewDidLoad() {
@@ -37,7 +37,7 @@ class DrawViewController: UIViewController,UICollectionViewDelegate, UICollectio
         opacitySlider.tintColor = .red
         featuresView.transform = CGAffineTransform(translationX: 0, y: kHeight - (kHeight - 80))
         currentView = canvasView
-        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureMovement))
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureMovement))
         currentView.addGestureRecognizer(panRecognizer)
     }
     @objc private func panGestureMovement(_ sender: UIPanGestureRecognizer) {
@@ -83,11 +83,12 @@ class DrawViewController: UIViewController,UICollectionViewDelegate, UICollectio
         
         // Change UIView Border Color to Red
         myNewView.layer.borderColor = UIColor.red.cgColor
+        currentView.removeGestureRecognizer(panRecognizer)
         currentView = myNewView
-        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureMovement))
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureMovement))
         currentView.addGestureRecognizer(panRecognizer)
         setCanvasValues()
-        self.view.insertSubview(myNewView, belowSubview: featuresView)
+        self.view.insertSubview(currentView, belowSubview: featuresView)
     }
     
     func setCanvasValues(){
--- Models/Drawing/CanvasView.swift
+++ Models/Drawing/CanvasView.swift
@@ -13,10 +13,12 @@ struct TouchPointsAndColor {
     var width: CGFloat?
     var opacity: CGFloat?
     var points: [CGPoint]
+    var eraser:Bool = false
     
     init(color: UIColor, points: [CGPoint]) {
         self.color = color
         self.points = points
+        
     }
 }
 
@@ -54,18 +56,64 @@ class CanvasView: UIView {
     }
     
     func userMovingLine(pointToBeAdded: CGPoint) {
-        guard var lastPoint = lines.popLast() else {return}
-        lastPoint.points.append(pointToBeAdded)
-        lastPoint.color = usingEraser ? .white :strokeColor
-        lastPoint.width = pencilStrokeWidth
-        lastPoint.opacity = pencilStrokeOpacity
-        lines.append(lastPoint)
-        checkifNeedToRemove()
+        if (usingEraser){
+            checkifNeedToRemove(pointToBeAdded: pointToBeAdded)
+        } else {
+            guard var lastPoint = lines.popLast() else {return}
+            lastPoint.points.append(pointToBeAdded)
+            lastPoint.color = strokeColor
+            lastPoint.width = pencilStrokeWidth
+            lastPoint.eraser = false
+            lastPoint.opacity =  usingEraser ? 0.0 : pencilStrokeOpacity
+            lines.append(lastPoint)
+        }
         setNeedsDisplay()
-   }
+    }
     
-    func checkifNeedToRemove(){
+    func distance(from: CGPoint, to: CGPoint) -> CGFloat {
+        return CGFloat(sqrt((from.x - to.x)*(from.x - to.x) + (from.y - to.y)*(from.y - to.y)))
+    }
+    
+    func isInCircle(withCenter center: CGPoint, point: CGPoint, radius: CGFloat) -> Bool {
+        return distance(from: center, to: point) <= radius
+    }
+    
+    func getPoints(withCenter center:CGPoint, radius: CGFloat) -> [CGPoint] {
+        let minX = Int(center.x - radius)
+        let minY = Int(center.y - radius)
+        let maxX = Int(center.x + radius)
+        let maxY = Int(center.y + radius)
         
+        var result = [CGPoint]()
+        for x in minX...maxX {
+            for y in minY...maxY {
+                let point = CGPoint(x: x, y: y)
+                if isInCircle(withCenter: center, point: point, radius: radius) {
+                    result.append(point)
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    
+    func checkifNeedToRemove(pointToBeAdded: CGPoint){
+        for checkIfNonEraseLine in 0..<lines.count{
+            if (!lines[checkIfNonEraseLine].eraser){
+                let pointsNearBy = getPoints(withCenter: pointToBeAdded, radius: pencilStrokeWidth * 20)
+                for pointNearBy in pointsNearBy{
+                    if (lines[checkIfNonEraseLine].points.contains(pointNearBy)){
+                        guard var lastPoint = lines.popLast() else {return}
+                        lastPoint.points.append(pointToBeAdded)
+                        lastPoint.color = .white
+                        lastPoint.width = pencilStrokeWidth
+                        lastPoint.opacity = pencilStrokeOpacity
+                        lines.append(lastPoint)
+                    }
+                }
+            }
+        }
     }
     
     func clearCanvasView() {
